33146971fdfd8d21b4d5f290b1051f3b140a8c40
z-jiuri <2061362927@qq.com>
2026-05-25 04:59:21 +0800

    更新run脚本

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,64 +1,51 @@
-exp_name=COST2100/out/seed3407_1e-4/base \
-train_path=/storage/hujiacong/zxd/datasets/cost2100/out_train.pt \
-val_path=/storage/hujiacong/zxd/datasets/cost2100/out_val.pt \
-test_path=/storage/hujiacong/zxd/datasets/cost2100/out_test.pt \
+exp_name=COST2100/in/transnet/base \
+train_path=/home/z-jiuri/workspace/Huawei/TransNet/data/COST2100/in_train.pt \
+val_path=/home/z-jiuri/workspace/Huawei/TransNet/data/COST2100/in_val.pt \
+test_path=/home/z-jiuri/workspace/Huawei/TransNet/data/COST2100/in_test.pt \
+encoder=transnet \
+code_adapter=false \
 batch_size=512 \
-lr_init=1e-4 \
-gpu=2 \
-seed=3407 \
-./scripts/train.sh > out_3407.log 2>&1 &
-
-exp_name=COST2100/out/seed42_1e-4/base \
-train_path=/storage/hujiacong/zxd/datasets/cost2100/out_train.pt \
-val_path=/storage/hujiacong/zxd/datasets/cost2100/out_val.pt \
-test_path=/storage/hujiacong/zxd/datasets/cost2100/out_test.pt \
-batch_size=512 \
-lr_init=1e-4 \
-gpu=2 \
+epochs=400 \
+lr_init=2e-4 \
+gpu=0 \
 seed=42 \
-./scripts/train.sh > out_42.log 2>&1 &
+./scripts/train.sh > in_transnet.log 2>&1 &
 
-exp_name=COST2100/out/seed2026_1e-4/base \
-train_path=/storage/hujiacong/zxd/datasets/cost2100/out_train.pt \
-val_path=/storage/hujiacong/zxd/datasets/cost2100/out_val.pt \
-test_path=/storage/hujiacong/zxd/datasets/cost2100/out_test.pt \
+exp_name=COST2100/in/clnet/base \
+train_path=/home/z-jiuri/workspace/Huawei/TransNet/data/COST2100/in_train.pt \
+val_path=/home/z-jiuri/workspace/Huawei/TransNet/data/COST2100/in_val.pt \
+test_path=/home/z-jiuri/workspace/Huawei/TransNet/data/COST2100/in_test.pt \
+encoder=clnet \
+code_adapter=true \
 batch_size=512 \
-lr_init=1e-4 \
-gpu=2 \
-seed=2026 \
-./scripts/train.sh > out_2026.log 2>&1 &
+epochs=400 \
+lr_init=2e-4 \
+gpu=0 \
+seed=42 \
+./scripts/train.sh > in_clnet.log 2>&1 &
 
-# exp_name=COST2100/in/seed42/base \
-# train_path=/storage/hujiacong/zxd/datasets/cost2100/in_train.pt \
-# val_path=/storage/hujiacong/zxd/datasets/cost2100/in_val.pt \
-# test_path=/storage/hujiacong/zxd/datasets/cost2100/in_test.pt \
-# batch_size=512 \
-# gpu=0 \
-# seed=42 \
-# ./scripts/train.sh > in_42.log 2>&1 &
+exp_name=COST2100/in/csinet/base \
+train_path=/home/z-jiuri/workspace/Huawei/TransNet/data/COST2100/in_train.pt \
+val_path=/home/z-jiuri/workspace/Huawei/TransNet/data/COST2100/in_val.pt \
+test_path=/home/z-jiuri/workspace/Huawei/TransNet/data/COST2100/in_test.pt \
+encoder=csinet \
+code_adapter=true \
+batch_size=512 \
+epochs=400 \
+lr_init=2e-4 \
+gpu=0 \
+seed=42 \
+./scripts/train.sh > in_csinet.log 2>&1 &
 
-# exp_name=COST2100/in/seed3407/base \
-# train_path=/storage/hujiacong/zxd/datasets/cost2100/in_train.pt \
-# val_path=/storage/hujiacong/zxd/datasets/cost2100/in_val.pt \
-# test_path=/storage/hujiacong/zxd/datasets/cost2100/in_test.pt \
-# batch_size=512 \
-# gpu=0 \
-# seed=3407 \
-# ./scripts/train.sh > in_3407.log 2>&1 &
-
-# exp_name=COST2100/in/seed2026/base \
-# train_path=/storage/hujiacong/zxd/datasets/cost2100/in_train.pt \
-# val_path=/storage/hujiacong/zxd/datasets/cost2100/in_val.pt \
-# test_path=/storage/hujiacong/zxd/datasets/cost2100/in_test.pt \
-# batch_size=512 \
-# gpu=0 \
-# seed=2026 \
-# ./scripts/train.sh > in_2026.log 2>&1 &
-
-
-
-
-
-
-
-
+exp_name=COST2100/in/crnet/base \
+train_path=/home/z-jiuri/workspace/Huawei/TransNet/data/COST2100/in_train.pt \
+val_path=/home/z-jiuri/workspace/Huawei/TransNet/data/COST2100/in_val.pt \
+test_path=/home/z-jiuri/workspace/Huawei/TransNet/data/COST2100/in_test.pt \
+encoder=crnet \
+code_adapter=true \
+batch_size=512 \
+epochs=400 \
+lr_init=2e-4 \
+gpu=0 \
+seed=42 \
+./scripts/train.sh > in_crnet.log 2>&1 &
